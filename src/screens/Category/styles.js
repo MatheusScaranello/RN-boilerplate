57d5991c7de4eb0e5f6ec578733102a527e6df4b
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-03-13 09:06:43 -0300

    Add Profile and Category screens to drawer and tab routes

--- src/screens/Category/styles.js
+++ src/screens/Category/styles.js
@@ -3,7 +3,36 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: "#f0f0f7",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  button: {
+    backgroundColor: "#8e44ad",
+    padding: 10,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
